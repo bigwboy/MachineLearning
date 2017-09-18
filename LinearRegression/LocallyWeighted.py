2701ee62ff4d5edc6de26e8b3ad8422f8cad1477
光光 <kevinliu830829@163.com>
2017-09-18 17:15:00 +0800

    局部加权算法

--- LinearRegression/LocallyWeighted.py
+++ LinearRegression/LocallyWeighted.py
@@ -5,20 +5,11 @@
 #Email:kevinliu830829@163.com
 from numpy import *
 
-def loadDataSet(fileName):
-    numFeat = len(open(fileName).readline().split('\t')) - 1
-    dataMat = []; labelMat = []
-    fr = open(fileName)
-    for line in fr.readlines():
-        lineArr =[]
-        curLine = line.strip().split('\t')
-        for i in range(numFeat):
-            lineArr.append(float(curLine[i]))
-        dataMat.append(lineArr)
-        labelMat.append(float(curLine[-1]))
-    return dataMat,labelMat
+
 def standRegres(xArr,yArr):
     xMat = mat(xArr)
+
+    #转置
     yMat = mat(yArr).T
     xTx = xMat.T * xMat
     if linalg.det(xTx) == 0.0:
@@ -26,6 +17,9 @@ def standRegres(xArr,yArr):
         return
     ws = xTx.I * (xMat.T * yMat)
     return ws
+
+
+
 def plotStandRegres(xArr,yArr,ws):
     import matplotlib.pyplot as plt
     fig = plt.figure()
@@ -37,11 +31,13 @@ def plotStandRegres(xArr,yArr,ws):
     yHat = xCopy*ws
     ax.plot([i[1] for i in xCopy],yHat)
     plt.show()
+
 def calcCorrcoef(xArr,yArr,ws):
     xMat = mat(xArr)
     yMat = mat(yArr)
     yHat = xMat*ws
     return corrcoef(yHat.T, yMat)
+
 def lwlr(testPoint,xArr,yArr,k=1.0):
     xMat = mat(xArr); yMat = mat(yArr).T
     m = shape(xMat)[0]
@@ -55,12 +51,14 @@ def lwlr(testPoint,xArr,yArr,k=1.0):
         return
     ws = xTx.I * (xMat.T * (weights * yMat))
     return testPoint * ws
+
 def lwlrTest(testArr,xArr,yArr,k=1.0):
     m = shape(testArr)[0]
     yHat = zeros(m)
     for i in range(m):
         yHat[i] = lwlr(testArr[i],xArr,yArr,k)
     return yHat
+
 def lwlrTestPlot(xArr,yArr,k=1.0):
     import matplotlib.pyplot as plt
     yHat = zeros(shape(yArr))
@@ -74,18 +72,36 @@ def lwlrTestPlot(xArr,yArr,k=1.0):
     ax.plot(xCopy,yHat)
     plt.show()
     #return yHat,xCopy
+
 def rssError(yArr,yHatArr): #yArr and yHatArr both need to be arrays
     return ((yArr-yHatArr)**2).sum()
 
 
+#数据输入
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t')) - 1
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr =[]
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
 
 
 #DEBUG
 if __name__ == "__main__":
-    # regression
-    xArr, yArr = loadDataSet('ex0.txt')
+    #数据输入
+    DATA=sys.argv[1]
+    xArr, yArr = loadDataSet(DATA)
+
+    #数据转置
     ws = standRegres(xArr, yArr)
     print ws
+
     # plotStandRegres(xArr,yArr,ws)
     print calcCorrcoef(xArr, yArr, ws)
     # lwlr
